f9e26946b66647318929c5cf8330dd0e8bcb9b10
Avie Seegobin <avieseegobin@gmail.com>
2015-09-15 16:16:42 -0400

    gulpfile tweaks 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 // include the required packages. 
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-  
+var gulp       = require('gulp');
+var stylus     = require('gulp-stylus');
+var fs         = require("fs");
+var browserify = require("browserify");
+var babelify   = require("babelify");
+
 // Get stylus .styl file and render 
 gulp.task('stylus', function () {
-  gulp.src('./css/chromestagram.styl')
+    gulp.src('./css/chromestagram.styl')
     .pipe(stylus())
     .pipe(gulp.dest('./dist'));
 });
- 
+
+gulp.task('bundler', function () {
+    browserify("./js/index.js", { debug: true })
+        .transform(babelify)
+        .bundle()
+        .on("error", function (err) { console.log("Error : " + err.message); })
+        .pipe(fs.createWriteStream("./dist/popup.js"));
+});
+
+
 gulp.task('compile-stylus', ['stylus']);
- 
+gulp.task('compile-ext', ['bundler','stylus']);
