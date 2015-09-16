0ea7dd60932a5f5892ce7790faf5cee7074f0dc4
Avie Seegobin <avieseegobin@gmail.com>
2015-09-16 19:25:13 -0400

    wip

--- js/components/grid.js
+++ js/components/grid.js
@@ -3,8 +3,10 @@ var React = require('react');
 var Grid = React.createClass({
     render: function() {
         return (
-            <div className='gram-grid'>
-                GRID
+            <div>
+                <div className='gram-grid'>
+                    GRID
+                </div>
             </div>
         );
     }
