894ba7abf987121ada759b7e00982e2dc08fde3e
Avie Seegobin <avieseegobin@gmail.com>
2015-09-17 15:25:30 -0400

    working http request for instagram feed

--- js/services/media_service.js
+++ js/services/media_service.js
@@ -1,20 +1,26 @@
 var jQuery = require('jQuery');
 
 function getFeed(token) {
-    var url = "https://api.instagram.com/v1/media/popular"
+    var url = "https://api.instagram.com/v1/media/popular";
+    
+    // var url = "https://api.instagram.com/v1/tags/toronto?access_token="+token;
+    
+    // var url = "https://api.instagram.com/v1/tags/coffee/media/recent";
+    // url += "?access_token=" + token;
+    // url += "&callback=" + jsonpCallback + ";";
     
     //return array of images
     jQuery.ajax({
-        method: "GET",
+        type: "GET",
         url: url,
         contentType: 'application/json; charset=utf-8',
         data: {
             access_token: token
         },
 
-        dataType: "jsonp",
-        cache: false,
-        jsonpCallback: 'jsonpCallback',
+        // dataType: "jsonp",
+        // cache: false,
+        // callback: 'jsonpCallback',
 
         success: function(results) {
             console.log(results)
@@ -25,9 +31,22 @@ function getFeed(token) {
         }
     });
 
+    // function jsonpCallback() {
+    //     console.log('callback');
+    // };
+
     // jQuery.getJSON(url, function(data) {
     //     console.log(data);
-    // })
+    // });
+    
+    // jQuery.ajax({
+    //     type: "GET",
+    //     dataType: "jsonp",
+    //     url: url, 
+    //     success: function(data) {
+    //         console.log(data);
+    //     }
+    // });
 }
 
 module.exports = {
